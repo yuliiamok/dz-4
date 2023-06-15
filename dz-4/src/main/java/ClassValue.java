c95ae7e23fffdac8e39285ed2dad6e4eed937d04
Yuliia Mokina <iuliia.mokina@gmail.com>
2023-06-15 16:29:39 +0200

    Added SimpleApplication.java, ClassValue.java and pom.xml

---
+++ dz-4/src/main/java/ClassValue.java
@@ -0,0 +1,8 @@
+public class ClassValue {
+
+    public int sampleValue = 1234;
+
+    public String getSampleValue() {
+        return "Значення поля: " + sampleValue;
+    }
+}
---
+++ dz-4/src/main/java/SimpleApplication.java
@@ -0,0 +1,8 @@
+public class SimpleApplication {
+    public static void main(String[] args) {
+
+    ClassValue value = new ClassValue();
+        System.out.println(value.getSampleValue());
+        System.out.println("Повернене значення: " + value.sampleValue);
+    }
+}
